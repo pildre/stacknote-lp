4b732b5edfd7c25947358bbde939f8cae383173f
小野 慶太 <dorokei@gmail.com>
2019-01-06 00:59:31 +0900

    Add material ui

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default () => (
   <div>
     <p>React Static is a progressive static site generator for React.</p>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   </div>
 );
--- static.config.js
+++ static.config.js
@@ -1,27 +1,40 @@
-import axios from 'axios'
+import axios from 'axios';
+// static.config.js
+import { createGenerateClassName } from '@material-ui/core/styles';
 
+const generateClassName = createGenerateClassName();
 export default {
+  plugins: [
+    [
+      'react-static-plugin-jss',
+      {
+        providerProps: {
+          generateClassName
+        }
+      }
+    ]
+  ],
   getSiteData: () => ({
-    title: 'React Static',
+    title: 'React Static'
   }),
   getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
-    )
+    );
     return [
       {
         path: '/blog',
         getData: () => ({
-          posts,
+          posts
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
           component: 'src/containers/Post',
           getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },
-}
+            post
+          })
+        }))
+      }
+    ];
+  }
+};
