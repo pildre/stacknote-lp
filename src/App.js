998f3067d21d25b16060a96a240e4f6a591e003a
小野 慶太 <dorokei@gmail.com>
2019-01-07 02:16:02 +0900

    Add external style sheet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Root, Routes } from 'react-static';
+import { Head, Root, Routes } from 'react-static';
 import { Link } from '@reach/router';
 import Header from 'components/Header/Header.jsx';
 import HeaderLinks from 'components/Header/HeaderLinks.jsx';
@@ -11,6 +11,17 @@ const dashboardRoutes = [];
 function App() {
   return (
     <Root>
+      <Head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
+        />
+        <link
+          href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+          rel="stylesheet"
+        />
+      </Head>
       <Header
         color="transparent"
         routes={dashboardRoutes}
