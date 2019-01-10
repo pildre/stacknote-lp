12bfabb3d823635b5bc39884a0bc9175a4c44686
小野 慶太 <dorokei@gmail.com>
2019-01-10 18:04:39 +0900

    Update blog page styles

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,12 +1,49 @@
-import React from 'react'
-import { withRouteData } from 'react-static'
-import { Link } from '@reach/router'
+import React from 'react';
+import { RouteData } from 'react-static';
+import { Link } from '@reach/router';
 
-export default withRouteData(({ post }) => (
-  <div>
-    <Link to="/blog/">{'<'} Back</Link>
-    <br />
-    <h3>{post.title}</h3>
-    <p>{post.body}</p>
-  </div>
-))
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+// @material-ui/core components
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import Header from 'components/Header/Header.jsx';
+import HeaderLinks from 'components/Header/HeaderLinks.jsx';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem.jsx';
+
+import postPageStyle from 'assets/jss/material-kit-react/views/postPage.jsx';
+
+class PostPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <RouteData>
+        {({ post }) => (
+          <div>
+            <Header
+              color="white"
+              brand="Material Kit React"
+              rightLinks={<HeaderLinks />}
+              fixed
+            />
+            <div className={classNames(classes.main, classes.mainRaised)}>
+              <div className={classes.container}>
+                <GridContainer spacing={16}>
+                  <GridItem xs={12}>
+                    <Link to="/blog/">{'<'} Back</Link>
+                    <br />
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                  </GridItem>
+                </GridContainer>
+              </div>
+            </div>
+          </div>
+        )}
+      </RouteData>
+    );
+  }
+}
+
+export default withStyles(postPageStyle)(PostPage);
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,33 +1,57 @@
 import React from 'react';
-import { withRouteData } from 'react-static';
+import { RouteData } from 'react-static';
 import { Link } from '@reach/router';
+
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+// @material-ui/core components
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import Header from 'components/Header/Header.jsx';
 import HeaderLinks from 'components/Header/HeaderLinks.jsx';
 import GridContainer from 'components/Grid/GridContainer.jsx';
 import GridItem from 'components/Grid/GridItem.jsx';
 
-export default withRouteData(({ posts }) => (
-  <div>
-    <Header
-      color="white"
-      brand="Material Kit React"
-      rightLinks={<HeaderLinks />}
-      fixed
-    />
-    <GridContainer spacing={16}>
-      <GridItem xs={12}>
-        <h1>It's blog time.</h1>
-        <br />
-        All Posts:
-        <ul>
-          {posts.map(post => (
-            <li key={post.id}>
-              <Link to={`/blog/post/${post.id}/`}>{post.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </GridItem>
-    </GridContainer>
-  </div>
-));
+import blogPageStyle from 'assets/jss/material-kit-react/views/blogPage.jsx';
+
+class BlogPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <RouteData>
+        {({ posts }) => (
+          <div>
+            <Header
+              color="white"
+              brand="Material Kit React"
+              rightLinks={<HeaderLinks />}
+              fixed
+            />
+            <div className={classNames(classes.main, classes.mainRaised)}>
+              <div className={classes.container}>
+                <GridContainer spacing={16}>
+                  <GridItem xs={12}>
+                    <h1>It's blog time.</h1>
+                    <br />
+                    All Posts:
+                    <ul>
+                      {posts.map(post => (
+                        <li key={post.id}>
+                          <Link to={`/blog/post/${post.id}/`}>
+                            {post.title}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </GridItem>
+                </GridContainer>
+              </div>
+            </div>
+          </div>
+        )}
+      </RouteData>
+    );
+  }
+}
+
+export default withStyles(blogPageStyle)(BlogPage);
