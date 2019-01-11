ff315d842546cedb104b568851b8881078c34df4
小野 慶太 <dorokei@gmail.com>
2019-01-12 00:19:02 +0900

    Add a main visual image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,28 @@ import { Link } from '@reach/router';
 import Button from 'components/CustomButtons/Button.jsx';
 import Header from 'components/Header/Header.jsx';
 import HeaderLinks from 'components/Header/HeaderLinks.jsx';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem.jsx';
+import Parallax from 'components/Parallax/Parallax.jsx';
 
 import { container } from 'assets/jss/material-kit-react.jsx';
 
 const indexPageStyle = {
-  container: {
-    ...container
+  container,
+  brand: {
+    color: '#FFFFFF',
+    textAlign: 'left'
+  },
+  title: {
+    fontSize: '4.2rem',
+    fontWeight: '600',
+    display: 'inline-block',
+    position: 'relative'
+  },
+  subtitle: {
+    fontSize: '1.313rem',
+    maxWidth: '500px',
+    margin: '10px 0 0'
   },
   main: {
     background: '#FFFFFF',
@@ -33,11 +49,27 @@ class IndexPage extends React.Component {
     return (
       <div className={classes.bg}>
         <Header
-          color="white"
-          brand="Material Kit React"
+          color="transparent"
+          brand="Stacknote"
           rightLinks={<HeaderLinks />}
           fixed
         />
+        <Parallax image={require('assets/img/colorful-colourful-paper.jpg')}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem>
+                <div className={classes.brand}>
+                  <h1 className={classes.title}>Stacknote</h1>
+                  <h3 className={classes.subtitle}>
+                    Stacknote is an application that can create long sentences
+                    by stacking short sentences like a chat application. It
+                    makes your writing surprisingly efficient.
+                  </h3>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
         <div className={classNames(classes.main)}>
           <div className={classes.container}>
             <h1>Welcome to React-Static</h1>
