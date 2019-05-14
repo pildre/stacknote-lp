7d588330982209474eedd02be541254380a07184
小野 慶太 <dorokei@gmail.com>
2019-05-15 00:19:53 +0900

    Try to add JssProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Head, Root, Routes } from 'react-static';
 import { Link } from '@reach/router';
+import JssProvider from 'react-jss/lib/JssProvider';
 
 import './app.scss';
 
@@ -8,22 +9,24 @@ const dashboardRoutes = [];
 
 function App() {
   return (
-    <Root>
-      <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
-        />
-        <link
-          href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="content">
-        <Routes />
-      </div>
-    </Root>
+    <JssProvider>
+      <Root>
+        <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
+          />
+          <link
+            href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <div className="content">
+          <Routes />
+        </div>
+      </Root>
+    </JssProvider>
   );
 }
 
