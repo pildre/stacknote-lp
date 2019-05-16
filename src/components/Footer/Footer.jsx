bfe1aaef804002ff36fbe7a49fae7cf3d81bd245
小野 慶太 <dorokei@gmail.com>
2019-05-17 00:52:44 +0900

    Change footer <a> tag to <Button> tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,9 @@
 /*eslint-disable*/
 import React from 'react';
-// nodejs library to set properties for components
 import PropTypes from 'prop-types';
-// nodejs library that concatenates classes
 import classNames from 'classnames';
 import { List, ListItem, withStyles } from '@material-ui/core';
-
-// @material-ui/icons
-import Favorite from '@material-ui/icons/Favorite';
+import Button from 'components/CustomButtons/Button.jsx';
 
 import Constants from 'lib/Constants';
 
@@ -29,24 +25,32 @@ function Footer({ ...props }) {
         <div className={classes.left}>
           <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
-              <a href="/terms" className={classes.block}>
-                Terms of Use
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a href="/privacy" className={classes.block}>
-                Privacy Policy
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                href="https://twitter.com/stacknote_app"
+              <Button
+                href="/terms"
+                color="transparent"
                 className={classes.block}
-                target="_blank"
+              >
+                Terms of Use
+              </Button>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <Button
+                href="/privacy"
+                color="transparent"
+                className={classes.block}
+              >
+                Privacy Policy
+              </Button>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <Button
+                href="https://twitter.com/stacknote_app"
+                color="transparent"
+                className={classes.block}
               >
                 <i className={classes.socialIcons + ' fab fa-twitter'} />
                 Twitter
-              </a>
+              </Button>
             </ListItem>
           </List>
         </div>
--- static.config.js
+++ static.config.js
@@ -9,7 +9,7 @@ export default {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="images/favicon.png" />
+        <link rel="shortcut icon" href="/images/favicon.png" />
         <link
           rel="stylesheet"
           type="text/css"
